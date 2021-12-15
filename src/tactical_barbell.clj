17f0011a2ce9fcf51bce0e79ba98cb2edbeb19e6
Adam Smith <1364177+adsmit14@users.noreply.github.com>
2021-12-15 05:23:58 -0700

    Clean up code

--- src/tactical_barbell.clj
+++ src/tactical_barbell.clj
@@ -7,22 +7,6 @@
            [java.time LocalDate]
            [java.time.format DateTimeFormatter]))
 
-(defn brzycki-formula [weight reps] (int (* weight (/ 36 (- 37 reps)))))
-
-
-(def lifts '(:bench :incline-bench :front-squat :back-squat :chin-ups :pull-ups :deadlift :press))
-
-(defn read-file [] (parse-stream (io/reader (io/resource "max.json")) keyword))
-(defn latest-max-day [data] (map #(hash-map (first %)
-                                            (dissoc (first (sort-by
-                                                             (fn [x] (LocalDate/parse (get x "date") (DateTimeFormatter/ofPattern "M/d/y")))
-                                                             (comp - compare) (second %))) "date")) data))
-
-(defn person-lift-maxes [max-day]
-  (into {} (map #(hash-map (first %)
-                           (into {} (map (fn [[k v]] {k (brzycki-formula (:weight v) (:reps v))})
-                                         (select-keys (second %) lifts)))) (apply merge max-day))))
-
 (def templates {:operator {70 {:sets 5 :reps 5}
                            80 {:sets 5 :reps 5}
                            90 {:sets 4 :reps 3}
@@ -30,25 +14,46 @@
                            85 {:sets 5 :reps 3}
                            95 {:sets 4 :reps 2}}})
 
-(def difficulty {:im-too-young-to-die #(Math/floor %)
-                 :hurt-me-plenty      #(Math/ceil %)})
+(def lifts '(:bench 
+              :incline-bench 
+              :front-squat 
+              :back-squat 
+              :chin-ups 
+              :pull-ups 
+              :deadlift 
+              :press))
 
-(defn calculate-weight [level percent max]
-  (int (* ((level difficulty) (Math/abs (float (/ (* max (/ percent 100)) 5)))) 5)))
+(defn brzycki-formula [weight reps] (int (* weight (/ 36 (- 37 reps)))))
 
-(defn weeks [maxes level template]
+(defn read-file [] (parse-stream (io/reader (io/resource "max.json")) keyword))
+
+(defn sort-by-date-descending [days] (sort-by
+                                       (fn [x] (LocalDate/parse (get x "date") (DateTimeFormatter/ofPattern "M/d/y")))
+                                       (comp - compare) days))
+
+(defn latest-max-day [data] (map #(let [person (first %) days (second %)]
+                                    (hash-map person (first (sort-by-date-descending days)))) data))
+
+(defn person-lift-maxes [max-day]
+  (into {} (map #(hash-map (first %)
+                           (into {} (map (fn [[k v]] {k (brzycki-formula (:weight v) (:reps v))})
+                                         (select-keys (second %) lifts)))) (apply merge max-day))))
+
+(defn calculate-weight [percent max]
+  (int (* (Math/floor (Math/abs (float (/ (* max (/ percent 100)) 5)))) 5)))
+
+(defn weeks [maxes template]
   (into (sorted-map) (apply merge (map (fn [[lifter lifts]]
                                          (hash-map lifter (map (fn [[percent routine]]
                                                                  (merge {} {:percent percent :sets-reps (str (:sets routine) "x" (:reps routine))}
-                                                                        (into {} (map (fn [[lift weight]] [lift (calculate-weight level percent weight)]) lifts))))
+                                                                        (into {} (map (fn [[lift weight]] [lift (calculate-weight percent weight)]) lifts))))
                                                                (template templates)))) maxes))))
 
 (defn -main
   "Generates Operator Block From Lastest Max Date"
   []
-  (let [w (weeks (person-lift-maxes (latest-max-day (read-file))) :im-too-young-to-die :operator)]
+  (let [w (weeks (person-lift-maxes (latest-max-day (read-file))) :operator)]
     (doseq [[lifter block] w]
       (println lifter)
       (pprint/print-table block)
       (println))))
-
